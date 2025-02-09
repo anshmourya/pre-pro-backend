2445e74064cc58c1c13f84f67ccfaf32e29c517f
anshMouryaa <ansh@levitation.co.in>
2025-02-09 17:03:14 +0530

    feat: Update video search to use user's tags for more relevant results

--- src/controller/dashboard.js
+++ src/controller/dashboard.js
@@ -1,16 +1,27 @@
 const youtube = require('../config/youtube');
+const User = require('../models/user');
 
 // @desc Get videos
 // @route GET /api/videos
 //with pagination
 const getVideos = async (req, res) => {
     try {
+        const { id: kindeId } = req.user;
+        const { limit = 30, pageToken = '' } = req.query;
+        const user = await User.findOne({ kindeId }).populate('tags');
+        if (!user) {
+            return res.notFound({
+                message: 'User not found'
+            })
+        }
+        const tags = user.tags.map(tag => tag.name);
+
         const response = await youtube.search.list({
             type: ['video'],
             part: ['snippet'],
-            q: 'indonesia',
-            maxResults: req.query.limit || 20,
-            pageToken: req.query.pageToken || ''
+            q: tags.join(' '),
+            maxResults: limit,
+            pageToken: pageToken
         });
         res.success({
             data: response.data
